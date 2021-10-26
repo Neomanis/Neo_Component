7a56df11a63ce145bc7b76e3ee6eb4e082b9ed78
apawlonski <Apawlonski@hotmail.com>
2021-10-26 14:44:44 +0200

    add i18n in app and storybook

---
+++ src/i18n.ts
@@ -0,0 +1,36 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import translationEN from "./locales/en/translation.json";
+import translationFR from "./locales/fr/translation.json";
+
+export const defaultLanguage = "en_GB";
+
+// the translations
+const resources = {
+    en_GB: {
+        translation: translationEN,
+    },
+    en_US: {
+        translation: translationEN,
+    },
+    fr_FR: {
+        translation: translationFR,
+    },
+};
+// the supported getGlpiLanguages NEEDS TO BE UPDATED WHEN NEW LANGUAGES ARE ADDED!!
+export const supportedLngs = ["fr_FR", "en_GB", "en_US"];
+
+i18n
+    // pass the i18n instance to react-i18next.
+    .use(initReactI18next)
+    // init i18next
+    // for all options read: https://www.i18next.com/overview/configuration-options
+    .init({
+        debug: false,
+        fallbackLng: defaultLanguage,
+        supportedLngs: supportedLngs,
+        resources: resources,
+    });
+
+export default i18n;
---
+++ src/locales/index.ts
@@ -0,0 +1,4 @@
+import translationEN from "./en/translation.json";
+import translationFR from "./fr/translation.json";
+
+export { translationEN, translationFR };
