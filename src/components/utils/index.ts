7f80722c90cf6f84dcee93127ab7de6b31257735
apawlonski <Apawlonski@hotmail.com>
2021-10-26 09:24:17 +0200

    add index for export function

---
+++ src/components/utils/index.ts
@@ -0,0 +1,12 @@
+import inputReducer from "./reducers/inputReducer";
+import { getStatusColor, getPriorityColor } from "./ticketColorSelector";
+import { getFormatedTimeToNow, getFormatedTimeToNowExtended, formatDate } from "./getFormatedTimeToNow";
+
+export {
+    getStatusColor,
+    getPriorityColor,
+    getFormatedTimeToNow,
+    getFormatedTimeToNowExtended,
+    inputReducer,
+    formatDate,
+};
