850cd2295651dd9ae13f98dc1e6d049e6b682504
apawlonski <Apawlonski@hotmail.com>
2021-11-12 15:31:16 +0100

    add getServiceStatusColor

--- src/components/utils/index.ts
+++ src/components/utils/index.ts
@@ -2,14 +2,16 @@ import inputReducer from "./reducers/inputReducer";
 import { getStatusColor, getPriorityColor } from "./ticketColorSelector";
 import { getFormatedTimeToNow, getFormatedTimeToNowExtended, formatDate } from "./getFormatedTimeToNow";
 import { displayRequesterName, getUserName, getRequesterUid } from "./displayRequesterName";
+import { getServiceStatusColor } from "./serviceStatusColorSelector";
 export {
+    displayRequesterName,
+    formatDate,
     getStatusColor,
     getPriorityColor,
     getFormatedTimeToNow,
     getFormatedTimeToNowExtended,
-    displayRequesterName,
-    formatDate,
     getUserName,
     getRequesterUid,
+    getServiceStatusColor,
     inputReducer,
 };
