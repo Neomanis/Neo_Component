a83bf049b7d8b242dec3adac3a85a094c259ffee
Déborah PALARD <deborah.plrd@gmail.com>
2021-11-12 16:31:18 +0100

    translations & placeholder in inputChat

--- src/components/molecules/chat/inputChat.tsx
+++ src/components/molecules/chat/inputChat.tsx
@@ -6,19 +6,20 @@ import { Input, Button } from "../../atoms";
 
 interface Props {
     cardOpen?: boolean;
+    placeholder: string;
     refForm: string;
     register?: UseFormRegister<FieldValues>;
     setValue?: UseFormSetValue<FieldValues>;
 }
 
-const InputChat = ({ cardOpen, refForm, register, setValue }: Props): ReactElement => {
+const InputChat = ({ cardOpen, placeholder, refForm, register, setValue }: Props): ReactElement => {
     return (
         <div className="flex items-center p-2 bg-neo_blue relative z-20">
             <Input
                 disabled={cardOpen}
                 inputClassName="bg-transparent w-full border-none text-neo_lite focus:outline-none"
                 isUpdateField={false}
-                placeholder="send"
+                placeholder={placeholder}
                 refForm={refForm}
                 register={register}
                 required
