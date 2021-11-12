98cbc7f17ecf6d6953c15576f631e2099627d20e
apawlonski <Apawlonski@hotmail.com>
2021-11-12 15:28:21 +0100

    fix

--- src/components/utils/index.ts
+++ src/components/utils/index.ts
@@ -1,16 +1,15 @@
 import inputReducer from "./reducers/inputReducer";
 import { getStatusColor, getPriorityColor } from "./ticketColorSelector";
 import { getFormatedTimeToNow, getFormatedTimeToNowExtended, formatDate } from "./getFormatedTimeToNow";
-import { displayRequesterName, getRequesterUid, getUserName } from "./displayRequesterName";
-
+import { displayRequesterName, getUserName, getRequesterUid } from "./displayRequesterName";
 export {
     getStatusColor,
     getPriorityColor,
     getFormatedTimeToNow,
     getFormatedTimeToNowExtended,
     displayRequesterName,
+    formatDate,
     getUserName,
     getRequesterUid,
     inputReducer,
-    formatDate,
 };
