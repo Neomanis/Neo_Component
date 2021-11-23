8bd17c97ef246a4a4010f9c39a3e5899e67d63e3
mschiavone <marco.sch4064@gmail.com>
2021-11-23 11:49:53 +0100

    classname on check box

--- src/components/atoms/checkBox.tsx
+++ src/components/atoms/checkBox.tsx
@@ -6,21 +6,23 @@ interface Props {
     fCallBack?: (e: ChangeEvent<HTMLInputElement>) => void;
     name?: string;
     testId?: string;
+    classNameInput?: string;
+    classNameLabel?: string;
 }
 
-const Checkbox = ({ checked, data, fCallBack, name, testId }: Props): ReactElement => {
+const Checkbox = ({ checked, data, fCallBack, name, testId, classNameInput, classNameLabel }: Props): ReactElement => {
     return (
         <>
             <input
                 checked={checked}
-                className="cursor-pointer"
+                className={`cursor-pointer ${classNameInput}`}
                 data-testid={testId}
                 id={name}
                 name={name}
                 onChange={fCallBack}
                 type="checkbox"
             />
-            <label htmlFor={name} className="mx-2 cursor-pointer">
+            <label htmlFor={name} className={`mx-2 cursor-pointer ${classNameLabel}`}>
                 {data}
             </label>
         </>
