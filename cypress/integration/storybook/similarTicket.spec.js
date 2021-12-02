b583ef76275567c8bc38e3fe713a3b8160e4ffdb
apawlonski <Apawlonski@hotmail.com>
2021-12-02 14:16:40 +0100

    fix

--- cypress/integration/storybook/similarTicket.spec.js
+++ cypress/integration/storybook/similarTicket.spec.js
@@ -6,7 +6,7 @@ describe("SimilarTicket", () => {
             "/iframe.html?id=molecules-ticket-similarticket--similar-ticket-default&args=&globals=locale:en_GB&viewMode=story"
         );
 
-        cy.get('[data-testId="ticketId"]').should("have.text", "Ticket 1");
-        cy.get('[data-testId="ticketName"]').should("have.text", "Test JM");
+        cy.get('[data-testid="ticketId"]').should("have.text", "Ticket 1");
+        cy.get('[data-testid="ticketName"]').should("have.text", "Test JM");
     });
 });
--- src/components/molecules/chat/inputChat.tsx
+++ src/components/molecules/chat/inputChat.tsx
@@ -31,13 +31,16 @@ const InputChat = ({
                     : "flex items-center bg-neo-bg-B relative text-white rounded-md divide-x-2 divide-neo-link"
             }`}
         >
+            <div className="p-2 opacity-50">
+                <AddFile fill="#7daab7" />
+            </div>
             <div className="flex items-center ">
                 <Input
                     disabled={cardOpen}
                     inputClassName={`${
                         inputClassName
                             ? inputClassName
-                            : "bg-transparent w-full border-none text-neo-light-grey focus:outline-none py-1 px-3"
+                            : "bg-transparent w-full border-none text-neo-light-grey focus:outline-none py-2 px-3"
                     }`}
                     isUpdateField={false}
                     placeholder={placeholder}
@@ -47,9 +50,6 @@ const InputChat = ({
                     setValue={setValue}
                     typeInput="text"
                 />
-            </div>
-            <div className="p-2 opacity-50">
-                <AddFile fill="#7daab7" />
             </div>
         </div>
     );
