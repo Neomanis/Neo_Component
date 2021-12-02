80a523ed457ad47d12d42909455c37c6971c19b1
apawlonski <Apawlonski@hotmail.com>
2021-12-02 12:09:53 +0100

    fix

--- src/components/molecules/chat/inputChat.tsx
+++ src/components/molecules/chat/inputChat.tsx
@@ -1,8 +1,7 @@
 import React, { ReactElement } from "react";
 import { FieldValues, UseFormRegister, UseFormSetValue } from "react-hook-form";
 import { ReactComponent as AddFile } from "../../../img/svg/nm_ico_addFile.svg";
-import { Input, Button } from "../../atoms";
-import { faPaperPlane } from "@fortawesome/free-regular-svg-icons";
+import { Input } from "../../atoms";
 
 interface Props {
     cardOpen?: boolean;
@@ -19,7 +18,6 @@ const InputChat = ({
     cardOpen,
     className,
     inputClassName,
-    buttonSub = false,
     placeholder,
     refForm,
     register,
@@ -33,16 +31,13 @@ const InputChat = ({
                     : "flex items-center bg-neo-bg-B relative text-white rounded-md divide-x-2 divide-neo-link"
             }`}
         >
-            <div className="p-2">
-                <AddFile fill="#7daab7" />
-            </div>
             <div className="flex items-center ">
                 <Input
                     disabled={cardOpen}
                     inputClassName={`${
                         inputClassName
                             ? inputClassName
-                            : "bg-transparent w-full border-none text-neo-light-grey focus:outline-none p-2"
+                            : "bg-transparent w-full border-none text-neo-light-grey focus:outline-none py-1 px-3"
                     }`}
                     isUpdateField={false}
                     placeholder={placeholder}
@@ -52,11 +47,9 @@ const InputChat = ({
                     setValue={setValue}
                     typeInput="text"
                 />
-                {buttonSub && (
-                    <div className="pr-2 transform hover:scale-105">
-                        <Button className="text-2xl" type="submit" fontIcon={faPaperPlane} />
-                    </div>
-                )}
+            </div>
+            <div className="p-2 opacity-50">
+                <AddFile fill="#7daab7" />
             </div>
         </div>
     );
