7ffbdb0cdd82ce2bf832a73f9941e9ad0b816d74
apawlonski <Apawlonski@hotmail.com>
2021-12-02 12:01:09 +0100

    update style inputChat

--- src/components/molecules/chat/inputChat.tsx
+++ src/components/molecules/chat/inputChat.tsx
@@ -1,33 +1,62 @@
 import React, { ReactElement } from "react";
 import { FieldValues, UseFormRegister, UseFormSetValue } from "react-hook-form";
-import { faPaperPlane } from "@fortawesome/free-regular-svg-icons";
-
+import { ReactComponent as AddFile } from "../../../img/svg/nm_ico_addFile.svg";
 import { Input, Button } from "../../atoms";
+import { faPaperPlane } from "@fortawesome/free-regular-svg-icons";
 
 interface Props {
     cardOpen?: boolean;
+    className?: string;
+    inputClassName?: string;
+    buttonSub?: boolean;
     placeholder: string;
     refForm: string;
     register?: UseFormRegister<FieldValues>;
     setValue?: UseFormSetValue<FieldValues>;
 }
 
-const InputChat = ({ cardOpen, placeholder, refForm, register, setValue }: Props): ReactElement => {
+const InputChat = ({
+    cardOpen,
+    className,
+    inputClassName,
+    buttonSub = false,
+    placeholder,
+    refForm,
+    register,
+    setValue,
+}: Props): ReactElement => {
     return (
-        <div className="flex items-center p-2 bg-neo-bg-B relative z-20 text-white">
-            <Input
-                disabled={cardOpen}
-                inputClassName="bg-transparent w-full border-none text-neo-light-grey focus:outline-none"
-                isUpdateField={false}
-                placeholder={placeholder}
-                refForm={refForm}
-                register={register}
-                required
-                setValue={setValue}
-                typeInput="text"
-            />
-            <div className="pl-1 transform hover:scale-105">
-                <Button className="text-2xl" type="submit" fontIcon={faPaperPlane} />
+        <div
+            className={`${
+                className
+                    ? className
+                    : "flex items-center bg-neo-bg-B relative text-white rounded-md divide-x-2 divide-neo-link"
+            }`}
+        >
+            <div className="p-2">
+                <AddFile fill="#7daab7" />
+            </div>
+            <div className="flex items-center ">
+                <Input
+                    disabled={cardOpen}
+                    inputClassName={`${
+                        inputClassName
+                            ? inputClassName
+                            : "bg-transparent w-full border-none text-neo-light-grey focus:outline-none p-2"
+                    }`}
+                    isUpdateField={false}
+                    placeholder={placeholder}
+                    refForm={refForm}
+                    register={register}
+                    required
+                    setValue={setValue}
+                    typeInput="text"
+                />
+                {buttonSub && (
+                    <div className="pr-2 transform hover:scale-105">
+                        <Button className="text-2xl" type="submit" fontIcon={faPaperPlane} />
+                    </div>
+                )}
             </div>
         </div>
     );
--- src/stories/molecules/chat/inputChat.stories.tsx
+++ src/stories/molecules/chat/inputChat.stories.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ComponentStory, Meta } from "@storybook/react";
-import { InputChat } from "../../..";
+import { InputChat, ITechnicalQuestion } from "../../..";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 export default {
     component: InputChat,
@@ -8,12 +9,26 @@ export default {
 } as Meta;
 
 const Template: ComponentStory<typeof InputChat> = (args) => {
+    const { setValue, register, handleSubmit } = useForm({ mode: "onChange" });
+    const onSubmit: SubmitHandler<ITechnicalQuestion> = async (data) => {
+        // eslint-disable-next-line no-console
+        console.log(data);
+    };
     return (
-        <div className="p-4 flex items-center w-1/4">
-            <InputChat {...args} />
+        <div className="w-full h-56 bg-neo-bg-A">
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="p-4 flex items-center ">
+                    <InputChat {...args} setValue={setValue} register={register} refForm="input" />
+                </div>
+            </form>
         </div>
     );
 };
 
 export const Default = Template.bind({});
 Default.args = {};
+
+export const Default2 = Template.bind({});
+Default2.args = {
+    buttonSub: true,
+};
