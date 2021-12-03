6566302a607ddf732a0bd59e6a9b8d6165be2c2a
tardigrade <l1ght3rth4nl1ght@gmail.com>
2021-12-03 17:14:19 +0100

    :white_check_mark: tabs tests

--- src/test/atoms/tabs/tabs.spec.js
+++ src/test/atoms/tabs/tabs.spec.js
@@ -1,12 +1,14 @@
 /* eslint-disable no-undef */
 
 import React from "react";
-import { Tabs } from "../../../components/atoms";
+import { Tabs, TabTitle } from "../../../components/atoms";
 import { mount } from "@cypress/react";
 import "../../../styles/tailwind.css";
 
-describe("Tab", () => {
+describe("Tabs", () => {
     it("should be visible and display correctly", () => {
-        mount(<Tabs />);
+        mount(<Tabs children={[<TabTitle title="Bye">Hello</TabTitle>, <TabTitle title="Re">Again</TabTitle>]} />);
+        cy.get('[data-testid="tabs-body"]').should("be.visible");
+        cy.get('[data-testid="tabTitle-body"]').should("be.visible");
     });
 });
