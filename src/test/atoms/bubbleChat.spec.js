33b655e2d7e283c1a339a625c180c4a593a0fe25
tardigrade <l1ght3rth4nl1ght@gmail.com>
2021-12-03 14:48:33 +0100

    :white_check_mark: bubble chat tests

---
+++ src/test/atoms/bubbleChat.spec.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-undef */
+
+import React from "react";
+import { BubbleChat } from "../../components/atoms";
+import { mount } from "@cypress/react";
+import "../../styles/tailwind.css";
+
+describe("BubbleChat", () => {
+    it("should be visible and display content props value", () => {
+        mount(<BubbleChat content="Helloworld" />);
+
+        cy.get('[data-testid="bubbleChat-body"]').should("be.visible").should("contain.text", "Helloworld");
+    });
+
+    it("should have background as bgColor props value", () => {
+        mount(<BubbleChat bgColor="bg-neo-bg-A" />);
+
+        cy.get('[data-testid="bubbleChat-body"]').should("have.class", "bg-neo-bg-A");
+    });
+
+    it("should not have background if bgColor props is not define", () => {
+        mount(<BubbleChat />);
+
+        cy.get('[data-testid="bubbleChat-body"]').should("not.have.class", "bg-neo-bg-A");
+    });
+
+    it("should show bubble chat border if border is define", () => {
+        mount(<BubbleChat border="true" />);
+
+        cy.get('[data-testid="bubbleChat-body"]').should("have.class", "border-2 border-neo-bg-B");
+    });
+
+    it("should not show bubble chat border if border is not define", () => {
+        mount(<BubbleChat />);
+
+        cy.get('[data-testid="bubbleChat-body"]').should("not.have.class", "bottom-6");
+    });
+});
