b5e22e4f48d884e4eabd852f7123cb6f7016e16a
apawlonski <Apawlonski@hotmail.com>
2021-12-06 17:02:18 +0100

    update svg index

--- src/img/svg/index.tsx
+++ src/img/svg/index.tsx
@@ -8,6 +8,7 @@ import { ReactComponent as CloseLogo } from "./nm_ico_close.svg";
 import { ReactComponent as ExpandLogo } from "./nm_ico_expand.svg";
 import { ReactComponent as TabLogo } from "./nm_ico_popup.svg";
 import { ReactComponent as IconAdd } from "./nm_ico_add.svg";
+import { ReactComponent as IconAddFill } from "./nm_ico_addFile.svg";
 import { ReactComponent as IconArrowLeft } from "./nm_ico_arrow_left.svg";
 import { ReactComponent as IconArrowRight } from "./nm_ico_arrow_right.svg";
 import { ReactComponent as IconChevron } from "./nm_ico_chevron.svg";
@@ -19,16 +20,22 @@ import { ReactComponent as IconHardwareSystem } from "./nm_ico_hardware-systems.
 import { ReactComponent as IconInbox } from "./nm_ico_inbox.svg";
 import { ReactComponent as IconInfo } from "./nm_ico_info.svg";
 import { ReactComponent as IconInventory } from "./nm_ico_inventory.svg";
+import { ReactComponent as IconKbOff } from "./nm_ico_kb_off.svg";
 import { ReactComponent as IconKbOn } from "./nm_ico_kb_on.svg";
+import { ReactComponent as IconKnowledge } from "./nm_ico_knowledge.svg";
 import { ReactComponent as IconLogout } from "./nm_ico_logout.svg";
 import { ReactComponent as IconMaintenance } from "./nm_ico_maintenance.svg";
+import { ReactComponent as LogoNeomanis } from "./nm_ico_neomanis.svg";
 import { ReactComponent as IconNotification } from "./nm_ico_notification.svg";
+import { ReactComponent as IconOther } from "./nm_ico_other.svg";
 import { ReactComponent as IconPrinter } from "./nm_ico_printer.svg";
 import { ReactComponent as IconEmptystar } from "./nm_ico_rating_emptystar.svg";
 import { ReactComponent as IconFullStar } from "./nm_ico_rating_fullstar.svg";
 import { ReactComponent as IconHalfStar } from "./nm_ico_rating_halfstar.svg";
 import { ReactComponent as IconRemoteControl } from "./nm_ico_remotecontrol.svg";
+import { ReactComponent as IconRemoteShell } from "./nm_ico_remoteshell.svg";
 import { ReactComponent as IconSearch } from "./nm_ico_search.svg";
+import { ReactComponent as IconSetting } from "./nm_ico_settings.svg";
 import { ReactComponent as IconStatusBar } from "./nm_ico_statusbar_long.svg";
 import { ReactComponent as IconStatusBarShort } from "./nm_ico_statusbar_short.svg";
 import { ReactComponent as IconSystemStatus } from "./nm_ico_systemstatus.svg";
@@ -41,8 +48,12 @@ import { ReactComponent as Planet } from "./nm_design_planet.svg";
 import { ReactComponent as TicketBottom } from "./nm_design_ticket_bottom.svg";
 import { ReactComponent as TicketCenter } from "./nm_design_ticket_center.svg";
 import { ReactComponent as TicketTop } from "./nm_design_ticket_top.svg";
-import { ReactComponent as RemoteShell } from "./nm_ico_remoteshell.svg";
-import { ReactComponent as RemoteControle } from "./nm_ico_remotecontrol.svg";
+import { ReactComponent as LogoBlanc } from "./nm_logo_couleur_blanc.svg";
+import { ReactComponent as LogoBLeu } from "./nm_logo_couleur_bleu.svg";
+import { ReactComponent as LogoBlancNoText } from "./nm_logo_couleur_blanc_sans_texte.svg";
+import { ReactComponent as LogoBleuNoText } from "./nm_logo_couleur_bleu_sans_texte.svg";
+import { ReactComponent as LogoMonoBlanc } from "./nm_logo_mono_blanc.svg";
+import { ReactComponent as LogoMonoNoir } from "./nm_logo_mono_noir.svg";
 
 export {
     TicketLogo,
@@ -58,6 +69,7 @@ export {
     ExpandLogo,
     TabLogo,
     IconAdd,
+    IconAddFill,
     IconArrowLeft,
     IconArrowRight,
     Planet,
@@ -73,20 +85,30 @@ export {
     IconInbox,
     IconInfo,
     IconInventory,
+    IconKbOff,
     IconKbOn,
+    IconKnowledge,
     IconLogout,
     IconMaintenance,
+    LogoNeomanis,
     IconNotification,
+    IconOther,
     IconPrinter,
     IconEmptystar,
     IconFullStar,
     IconHalfStar,
     IconRemoteControl,
+    IconRemoteShell,
     IconSearch,
+    IconSetting,
     IconStatusBar,
     IconStatusBarShort,
     IconSystemStatus,
     IconTicketClosed,
-    RemoteShell,
-    RemoteControle,
+    LogoBlanc,
+    LogoBLeu,
+    LogoBlancNoText,
+    LogoBleuNoText,
+    LogoMonoBlanc,
+    LogoMonoNoir,
 };
