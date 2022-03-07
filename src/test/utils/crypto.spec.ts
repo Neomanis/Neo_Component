700795154c1e439f56e74b4f59cd10d6046a8b21
mschiavone <marco.sch4064@gmail.com>
2022-03-07 12:13:30 +0100

    test jsEncrypt

---
+++ src/test/utils/crypto.spec.ts
@@ -0,0 +1,14 @@
+import JSEncrypt from "jsencrypt";
+import { frontEncrypt } from "../../components/utils/crypto/frontUtils";
+import { publicKey, privateKey } from "../keys-pair/keys";
+
+const password = "Konbawa";
+describe("JSEncrypt", () => {
+    it("Should encrypt", () => {
+        const encrypted = frontEncrypt(password, publicKey);
+        expect(encrypted).to.not.equal(password);
+        const jsEncrypt = new JSEncrypt();
+        jsEncrypt.setPrivateKey(privateKey);
+        expect(jsEncrypt.decrypt(encrypted)).to.equal(password);
+    });
+});
