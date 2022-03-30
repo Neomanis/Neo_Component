bc1167fcd80dbcdafa9ba3dda2e82cbf877f8f69
Déborah PALARD <deborah.plrd@gmail.com>
2022-03-30 12:07:55 +0200

    update refName

--- src/stories/molecules/shadowBoxWrapper.stories.tsx
+++ src/stories/molecules/shadowBoxWrapper.stories.tsx
@@ -9,10 +9,10 @@ export default {
 } as Meta;
 
 const Template: ComponentStory<typeof ShadowBoxWrapper> = (args) => {
-    const refP = useRef<HTMLUListElement>(null);
+    const refParent = useRef<HTMLUListElement>(null);
     return (
         <div className="bg-neo-bg-A p-3 h-32 flex items-center relative" style={{ width: "300px" }}>
-            <ShadowBoxWrapper refParent={refP} {...args}>
+            <ShadowBoxWrapper refParent={refParent} {...args}>
                 <li className="bg-neo-bg-B h-8 flex justify-center items-center p-2">Item 1</li>
                 <li className="bg-neo-bg-B h-8 flex justify-center items-center p-2">Item 4</li>
                 <li className="bg-neo-bg-B h-8 flex justify-center items-center p-2">Item 8</li>
