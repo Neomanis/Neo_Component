13bb0c18271a8f749e72359c1f43ec94e47059ae
Roxxouille <raphael.huiban.le.corff@gmail.com>
2022-04-04 09:56:29 +0200

    :alien: updated chatCard and input choice due to bot v2 change

--- src/components/molecules/chat/chatCard.tsx
+++ src/components/molecules/chat/chatCard.tsx
@@ -1,63 +1,17 @@
 import React, { ReactElement } from "react";
-import { Title, InputSelectSearchable, InputChoice } from "../../atoms";
+import { InputChoice } from "../../atoms";
 
 interface Props {
-    datas: { label: string; value: string }[];
-    fCallBack: (data: { text: string; value: string }) => void;
-    placeholder?: string;
-    title: string;
-    type: string;
+    data: { label: string; value: number }[];
+    fCallBack: (data: { label: string; value: number }) => void;
 }
 
-const ChatCard = ({ datas, fCallBack, placeholder, title, type }: Props): ReactElement => {
-    let option;
-    switch (type) {
-        case "dropdown":
-            option = (
-                <>
-                    <Title className={"text-white"} type="h3" data={title} />
-                    <InputSelectSearchable
-                        refForm="dropdown"
-                        isSearchable
-                        containerClassName={"mt-2"}
-                        data={datas.map((data) => ({ label: data.label, value: parseInt(data.value) }))}
-                        setStateValue={(data) =>
-                            fCallBack({
-                                value: data.toString(),
-                                text: datas.find((option) => option.value === data.toString())?.label,
-                            })
-                        }
-                        placeholder={placeholder}
-                    />
-                </>
-            );
-            break;
-        case "single-choice":
-            option = (
-                <div className="px-4 py-1">
-                    <Title className={"text-white"} type="h3" data={title} />
-                    <InputChoice
-                        className={"my-1"}
-                        data={datas}
-                        fCallBack={(data) =>
-                            fCallBack({
-                                value: data,
-                                text: datas.find((choice) => choice.value === data)?.label,
-                            })
-                        }
-                    />
-                </div>
-            );
-            break;
-
-        default:
-            option = null;
-            break;
-    }
-
+const ChatCard = ({ data, fCallBack }: Props): ReactElement => {
     return (
         <div className={`rounded-sm bg-neo-bg-B p-2 border-b-2 border-neo-expanded overflow-hidden`}>
-            <div>{option}</div>
+            <div className="px-4 py-1">
+                <InputChoice className={"my-1"} data={data} fCallBack={(data) => fCallBack(data)} />
+            </div>
         </div>
     );
 };
