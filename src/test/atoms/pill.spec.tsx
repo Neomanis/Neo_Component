f9c53bf9f81b12be6bbbdb7256a311884e106c3b
Jeremy <51856630+Xelorass@users.noreply.github.com>
2022-04-08 16:45:16 +0200

    :white_check_mark: pill test 100%

--- src/test/atoms/pill.spec.tsx
+++ src/test/atoms/pill.spec.tsx
@@ -5,6 +5,12 @@ describe("Pill", () => {
     it("should be visible ", () => {
         mount(<Pill data="Helloworld" />);
 
-        cy.get('[data-testid="pill-body"]').should("be.visible");
+        cy.get('[data-testid="pill-body"]').should("exist").should("be.visible");
+    });
+
+    it("should display CSS correclty if prop className exist ", () => {
+        mount(<Pill data="Helloworld" className="bg-neo-light-grey" />);
+
+        cy.get('[data-testid="pill-body"]').should("have.css", "background-color").and("eq", "rgb(218, 229, 229)");
     });
 });
