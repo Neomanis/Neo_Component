798ef7140e4948b68d283fa8d23831d1fcf04b37
Jeremy <51856630+Xelorass@users.noreply.github.com>
2022-06-28 15:15:44 +0200

    :sparkles: add formatOptionLabel param in InputSelectSearchable

--- src/components/atoms/input/inputSelectSearchable.tsx
+++ src/components/atoms/input/inputSelectSearchable.tsx
@@ -7,6 +7,7 @@ import { ReactHookFormCustomValidation } from "@neomanis/neo-types";
 import Updater from "../updater";
 import { SelectComponents } from "react-select/dist/declarations/src/components";
 import { useTranslation } from "@neomanis/neo-translation";
+import { FormatOptionLabelMeta } from "react-select/dist/declarations/src/Select";
 
 interface Props {
     containerClassName?: string;
@@ -34,6 +35,18 @@ interface Props {
     doValueLogic?: boolean;
     dotClassName?: string;
     errorMessage?: string;
+    formatOptionLabel?: (
+        data: {
+            label: string;
+            value: number;
+            icon?: ReactElement;
+        },
+        formatOptionLabelMeta: FormatOptionLabelMeta<{
+            label: string;
+            value: number;
+            icon?: ReactElement;
+        }>
+    ) => React.ReactNode;
     id?: string;
     isClearable?: boolean;
     isError?: boolean;
@@ -63,6 +76,7 @@ const InputSelectSearchable = ({
     doValueLogic = true,
     dotClassName,
     errorMessage,
+    formatOptionLabel,
     isClearable = false,
     isError,
     isMulti = false,
@@ -229,6 +243,7 @@ const InputSelectSearchable = ({
                 </div>
             )}
             <Select
+                formatOptionLabel={formatOptionLabel}
                 className="flex items-center w-full rounded-md text-xs font-bold"
                 isSearchable={isSearchable}
                 styles={overrideBaseCustomStyle(customStyles, customStyleOverride)}
