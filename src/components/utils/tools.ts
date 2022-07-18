b80d085a7d1360e0b6b8dd8d2a6eae71cb1e9daa
Déborah PALARD <deborah.plrd@gmail.com>
2022-07-18 17:36:52 +0200

    add settings dropdown colors

--- src/components/utils/tools.ts
+++ src/components/utils/tools.ts
@@ -108,6 +108,10 @@ export function getHexColorFromTailwindColor(tailwindColor: string): string | un
                 grey: "#626574",
                 purple: "#5715C6",
             },
+            settings: {
+                grey: "#313235",
+                lightGrey: "#4A4B4E",
+            },
         },
     };
     tailwindColor.split("-").forEach((color) => {
