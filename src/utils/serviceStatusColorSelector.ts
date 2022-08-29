87746672e4fd76c7a9e7d784f9c07e57141afa30
mschiavone <marco.sch4064@gmail.com>
2022-08-29 14:52:10 +0200

    update getStatusColor function

--- src/utils/serviceStatusColorSelector.ts
+++ src/utils/serviceStatusColorSelector.ts
@@ -1,14 +1,31 @@
-export function getServiceStatusColor(status: number | undefined): string {
-    switch (status) {
-        case 200:
-            return "green-400";
-        case 202:
-            return "blue-500";
-        case 500:
-            return "yellow-400";
-        case 503:
-            return "red-500";
-        default:
-            return "yellow-600";
+export function getServiceStatusColor(tailwindProperty: "bg" | "text", status: number | undefined): string {
+    if (tailwindProperty === "bg") {
+        switch (status) {
+            case 200:
+                return "bg-green-400";
+            case 202:
+                return "bg-blue-500";
+            case 500:
+                return "bg-yellow-400";
+            case 503:
+                return "bg-red-500";
+            default:
+                return "bg-yellow-600";
+        }
+    } else if (tailwindProperty === "text") {
+        switch (status) {
+            case 200:
+                return "text-green-400";
+            case 202:
+                return "text-blue-500";
+            case 500:
+                return "text-yellow-400";
+            case 503:
+                return "text-red-500";
+            default:
+                return "text-yellow-600";
+        }
+    } else {
+        return "undefined";
     }
 }
