f3f98ec8bd8c1c6f4afb832c06b3994df56d6208
mschiavone <marco.sch4064@gmail.com>
2022-10-06 15:31:50 +0200

    add userWatcher function

--- src/components/molecules/Ticket/Ticket.tsx
+++ src/components/molecules/Ticket/Ticket.tsx
@@ -54,15 +54,22 @@ const Ticket = ({
         );
     }
 
-    function checkIsWatcher(): boolean {
-        if (ticket && userGroups && userNeoId) {
+    function checkIsGroupWatcher(): boolean {
+        if (ticket && userGroups) {
             const groupFound = userGroups.filter(
                 (userGroup) =>
-                    ticket.groupWatcher.map((group) => group.id).includes(userGroup.id) &&
-                    ticket.groupWatcher.map((group) => group.name).includes(userGroup.name) &&
-                    ticket.groupWatcher.map((group) => group.itsmCode).includes(userGroup.itsmCode)
+                    Boolean(ticket.groupWatcher.map((group) => group.id).includes(userGroup.id)) &&
+                    Boolean(ticket.groupWatcher.map((group) => group.name).includes(userGroup.name)) &&
+                    Boolean(ticket.groupWatcher.map((group) => group.itsmCode).includes(userGroup.itsmCode))
             );
             return Boolean(groupFound[0]);
+        }
+        return false;
+    }
+
+    function checkIsUserWatcher(): boolean {
+        if (ticket && userNeoId) {
+            return Boolean(ticket.userWatcher.find((watcherNeoId) => watcherNeoId === userNeoId));
         }
         return false;
     }
@@ -91,7 +98,7 @@ const Ticket = ({
             data-testid="ticket-body"
         >
             <div className="absolute w-full" style={{ zIndex: 3 }}>
-                {checkIsWatcher() && (
+                {(checkIsGroupWatcher() || checkIsUserWatcher()) && (
                     <div className="h-8 w-8 absolute left-6 top-3" data-testid="ticket-icon-watcher">
                         <IconWatcherBlue className="absolute left-0 transform scale-110 w-6 fill-white" />
                     </div>
