336f9fa2d0e602a6a95686eded17acbdcab87f1e
Alex <Apawlonski@hotmail.com>
2022-10-28 14:13:57 +0200

    update MessageChat and BubbleChat to add isFailed, isValidate and isLoading state

--- src/components/atoms/BubbleChat/BubbleChat.tsx
+++ src/components/atoms/BubbleChat/BubbleChat.tsx
@@ -3,13 +3,18 @@ import React, { ReactElement } from "react";
 export interface BubbleChatProps {
     bgColor?: string;
     border?: string;
+    isValidate?: boolean;
     content: string | ReactElement;
 }
 
-const BubbleChat = ({ bgColor, border, content }: BubbleChatProps): ReactElement => {
+const BubbleChat = ({ bgColor, border, content, isValidate = true }: BubbleChatProps): ReactElement => {
     return (
         <div
-            className={`${bgColor} ${border} ${border && "border-2"} text-xxs rounded-md p-2 text-neo-light-grey`}
+            className={`text-xxs rounded-md p-2 text-neo-light-grey 
+            ${bgColor ? bgColor : ""} 
+            ${border ? border : ""} 
+            ${border ? "border-2" : ""} 
+            ${isValidate ? "" : "opacity-50"}`}
             data-testid="bubbleChat-body"
         >
             {content}
