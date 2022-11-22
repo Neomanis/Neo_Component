544fbcde121eae83e3d2c56cc14b6a83e70ec884
Déborah PALARD <deborah.plrd@gmail.com>
2022-11-22 16:09:44 +0100

    add white sad neohelper logo for empty neoHelperScreen

--- src/img/png/index.tsx
+++ src/img/png/index.tsx
@@ -3,6 +3,7 @@ import LogoBleuCarre from "./logo_bleu_carre.png";
 import LogoBlanc from "./nm_logo_couleur_blanc_sans_texte.png";
 import LogoBleu from "./logo_bleu.png";
 import LogoFullBlanc from "./nm_logo_mono_blanc.png";
+import NeoLogoSadWhite from "./nm_logo_couleur_blanc_toast-sad-white.png";
 import QueueIconEmpty from "./queue_Icon_empty.png";
 
-export { DefaultUserPicture, LogoBleuCarre, LogoBlanc, LogoBleu, LogoFullBlanc, QueueIconEmpty };
+export { DefaultUserPicture, LogoBleuCarre, LogoBlanc, LogoBleu, LogoFullBlanc, NeoLogoSadWhite, QueueIconEmpty };
