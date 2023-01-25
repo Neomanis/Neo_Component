f59215a25de1ab390baddab600bdacc0a8948d6a
Jeremy <51856630+Xelorass@users.noreply.github.com>
2023-01-25 11:15:50 +0100

    :safety_vest: refactor code to prettier it

--- src/components/atoms/AttachmentChat/AttachmentChat.tsx
+++ src/components/atoms/AttachmentChat/AttachmentChat.tsx
@@ -43,37 +43,36 @@ const AttachmentChat = ({
                     <FontAwesomeIcon icon={faFileDownload} className="mr-1 text-2xl text-neo-blue mr-2" />
                     <div
                         className="text-xxs break-words line-clamp-2 hover:underline cursor-pointer w-4/5"
-                        onClick={() => {
-                            downloadCallback(attachmentId);
-                        }}
+                        onClick={() => downloadCallback(attachmentId)}
                     >
-                        {content}
+                        <p>{content}</p>
                     </div>
                     <IconTrash
                         className={classNames(
                             "fill-neo-link opacity-20 w-3 ml-2 cursor-pointer",
                             "hover:fill-neo-red hover:opacity-100"
                         )}
-                        onClick={() => {
-                            setOpenValidationCard(true);
-                        }}
+                        onClick={() => setOpenValidationCard(true)}
                     />
                 </div>
             ) : (
                 <ValidationCard
                     classNames={{
-                        container: `flex flex-row justify-between items-center relative rounded-md p-2  ${border} ${
-                            border && "border-2"
-                        } ${bgColor}`,
+                        container: classNames(
+                            "flex flex-row justify-between items-center relative rounded-md p-2",
+                            border,
+                            border && "border-2",
+                            bgColor
+                        ),
                         buttonContainer: "flex justify-around w-1/4 ml-1",
                         text: "text-xxs text-white",
                     }}
-                    fCallBackCancel={(): void => setOpenValidationCard(false)}
-                    fCallBackValidate={(): void => {
+                    fCallBackCancel={() => setOpenValidationCard(false)}
+                    fCallBackValidate={() => {
                         setOpenValidationCard(false);
                         deleteCallback(attachmentId);
                     }}
-                    text={t("global.deleteThis") + " " + t("ticket.attachment", { count: 1 }).toLocaleLowerCase() + "?"}
+                    text={`${t("global.deleteThis")} ${t("ticket.attachment", { count: 1 }).toLocaleLowerCase()} ?`}
                     id="delete-button"
                 />
             )}
