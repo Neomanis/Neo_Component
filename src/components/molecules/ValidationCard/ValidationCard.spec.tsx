7010199327420803dc5dba7558c406de302357d2
Jeremy <51856630+Xelorass@users.noreply.github.com>
2023-02-16 10:55:39 +0100

    :white_check_mark: test ValidationCard

--- src/components/molecules/ValidationCard/ValidationCard.spec.tsx
+++ src/components/molecules/ValidationCard/ValidationCard.spec.tsx
@@ -8,3 +8,24 @@ test("should work", async ({ mount }) => {
     const component = await mount(<ValidationCard fCallBackCancel={() => {}} fCallBackValidate={() => {}} />);
     await expect(component).toBeVisible();
 });
+
+test("should launch callback", async ({ mount }) => {
+    let validateClick = false;
+    let cancelClick = false;
+
+    const component = await mount(
+        <ValidationCard
+            fCallBackCancel={() => {
+                cancelClick = true;
+            }}
+            fCallBackValidate={() => {
+                validateClick = true;
+            }}
+        />
+    );
+
+    await component.locator('[data-testid="on-click-validate"]').click();
+    expect(validateClick).toBe(true);
+    await component.locator('[data-testid="on-click-cancel"]').click();
+    expect(cancelClick).toBe(true);
+});
