e72b9f95fb9f76ecbda613e80e014f7355e47c5e
Jeremy <51856630+Xelorass@users.noreply.github.com>
2023-03-03 10:11:41 +0100

    :lipstick: modify TQ-tab component

--- src/components/molecules/TechnicalQuestionItem/TechnicalQuestionItem.spec.tsx
+++ src/components/molecules/TechnicalQuestionItem/TechnicalQuestionItem.spec.tsx
@@ -4,72 +4,87 @@ import TechnicalQuestionItem from "./TechnicalQuestionItem";
 
 test.use({ viewport: { width: 500, height: 500 } });
 
-test("action should works", async ({ mount }) => {
+const fakePartialTicket = {
+    id: 0,
+    uid: "1gl-666-INC",
+    priority: 0,
+    status: 6,
+};
+
+test("component should exist and action should works", async ({ mount }) => {
     let open = false;
     const component = await mount(
         <TechnicalQuestionItem
             answerAmount={1}
             createDate={"2021-11-15T13:34:48.551Z"}
             id={2}
-            isSelected={false}
+            selectedQuestion={undefined}
             openTechnicalQuestion={() => (open = true)}
             solved={false}
-            ticket={{
-                id: 0,
-                uid: "1gl-666-INC",
-                priority: 0,
-                status: 6,
-            }}
+            ticket={fakePartialTicket}
             title={"Title"}
         />
     );
 
     await component.click();
     expect(open).toBe(true);
+    await expect(component.locator('[data-testid="tq-middle"]')).toHaveClass(/opacity-100/);
+    await expect(component.locator('[data-testid="tq-end"]')).toBeDefined;
+    await expect(component.locator('[data-testid="tq-end"]')).toBeVisible;
 });
 
-test("should display content correctly if not selected", async ({ mount }) => {
+test("should display content correctly if not selected and/or not solved", async ({ mount }) => {
     const component = await mount(
         <TechnicalQuestionItem
             answerAmount={1}
             createDate={"2021-11-15T13:34:48.551Z"}
             id={2}
-            isSelected={false}
+            selectedQuestion={1}
             openTechnicalQuestion={() => {}}
             solved={false}
-            ticket={{
-                id: 0,
-                uid: "1gl-666-INC",
-                priority: 0,
-                status: 6,
-            }}
+            ticket={fakePartialTicket}
             title={"Title"}
         />
     );
 
-    await expect(component.locator('[data-testid="tq-middle"]')).toHaveClass(/bg-neo-bg-B/);
+    await expect(component.locator('[data-testid="tq-middle"]')).toHaveClass(/opacity-50/);
+    await expect(component.locator('[data-testid="tq-pill"]')).toHaveClass(/bg-neo-red/);
     await expect(component.locator('[data-testid="tq-middle-bottom"]')).toHaveClass(/text-neo-blue-secondary/);
 });
 
-test("should display content correctly if selected", async ({ mount }) => {
+test("should display content correctly if selected or/and solved", async ({ mount }) => {
     const component = await mount(
         <TechnicalQuestionItem
             answerAmount={1}
             createDate={"2021-11-15T13:34:48.551Z"}
             id={2}
-            isSelected
+            selectedQuestion={2}
             openTechnicalQuestion={() => {}}
-            solved={false}
-            ticket={{
-                id: 0,
-                uid: "1gl-666-INC",
-                priority: 0,
-                status: 6,
-            }}
+            solved={true}
+            ticket={fakePartialTicket}
             title={"Title"}
         />
     );
 
-    await expect(component.locator('[data-testid="tq-middle"]')).toHaveClass(/bg-neo-blue/);
+    await expect(component.locator('[data-testid="tq-middle"]')).toHaveClass(/opacity-100/);
+    await expect(component.locator('[data-testid="tq-pill"]')).toHaveClass(/bg-neo-green/);
     await expect(component.locator('[data-testid="tq-middle-bottom"]')).toHaveClass(/text-white/);
+});
+
+test("shouldn't display information not passed", async ({ mount }) => {
+    const component = await mount(
+        <TechnicalQuestionItem
+            answerAmount={undefined}
+            createDate={undefined}
+            id={2}
+            selectedQuestion={2}
+            openTechnicalQuestion={() => {}}
+            solved={false}
+            title={"Title"}
+        />
+    );
+
+    await expect(component.locator('[data-testid="tq-end"]')).toBeUndefined;
+    await expect(component.locator('[data-testid="tq-date"]')).toBeUndefined;
+    await expect(component.locator('[data-testid="tq-answer"]')).toBeUndefined;
 });
--- src/components/molecules/TechnicalQuestionItem/TechnicalQuestionItem.stories.tsx
+++ src/components/molecules/TechnicalQuestionItem/TechnicalQuestionItem.stories.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable no-console */
-import React from "react";
+import React, { useState } from "react";
 import { ComponentStory, Meta } from "@storybook/react";
 
 import TechnicalQuestionItem from "./TechnicalQuestionItem";
+import { Button } from "@/components/atoms";
 
 export default {
     component: TechnicalQuestionItem,
@@ -12,32 +13,81 @@ export default {
 const Template: ComponentStory<typeof TechnicalQuestionItem> = (args) => {
     return <TechnicalQuestionItem {...args} />;
 };
+const fakePartialTicket = {
+    uid: "[GL1]-INC-666",
+    priority: 4,
+    status: 2,
+    id: 1,
+};
+
+const TemplateList: ComponentStory<typeof TechnicalQuestionItem> = (args) => {
+    const [selectedQuestion, setSelectedQuestion] = useState<number>(2);
+
+    return (
+        <div className="flex flex-row items-center">
+            <div>
+                <TechnicalQuestionItem
+                    id={1}
+                    solved={true}
+                    selectedQuestion={selectedQuestion}
+                    openTechnicalQuestion={() => setSelectedQuestion(1)}
+                    ticket={fakePartialTicket}
+                    {...args}
+                />
+                <TechnicalQuestionItem
+                    id={2}
+                    solved={false}
+                    selectedQuestion={selectedQuestion}
+                    openTechnicalQuestion={() => setSelectedQuestion(2)}
+                    ticket={undefined}
+                    {...args}
+                />
+                <TechnicalQuestionItem
+                    id={3}
+                    solved={true}
+                    selectedQuestion={selectedQuestion}
+                    openTechnicalQuestion={() => setSelectedQuestion(3)}
+                    ticket={fakePartialTicket}
+                    {...args}
+                />
+            </div>
+            <Button className="h-10" onClick={() => setSelectedQuestion(undefined)}>
+                No selection
+            </Button>
+        </div>
+    );
+};
 
 export const Default: ComponentStory<typeof TechnicalQuestionItem> = Template.bind({});
 Default.args = {
     answerAmount: 7,
-    createDate: "2021-11-15T13:34:48.551Z",
+    createDate: "2023-01-02T13:34:48.551Z",
     id: 2,
-    isSelected: false,
     openTechnicalQuestion: () => console.log("yo"),
     solved: true,
     ticket: {
         uid: "[GL1]-INC-666",
-        priority: 1,
-        status: 5,
+        priority: 4,
+        status: 2,
         id: 1,
     },
     title: "You strive for victory",
 };
 
-export const TechnicalQuestionItemTicketNull = Template.bind({});
-TechnicalQuestionItemTicketNull.args = {
+export const TicketNull = Template.bind({});
+TicketNull.args = {
     answerAmount: 1,
-    createDate: "2021-11-15T13:34:48.551Z",
+    createDate: "2023-02-04T13:34:48.551Z",
     id: 2,
-    isSelected: false,
     openTechnicalQuestion: () => console.log("yo"),
-    solved: true,
+    solved: false,
     ticket: undefined,
+    title: "You strive for victory",
+};
+
+export const QuestionSelection = TemplateList.bind({});
+QuestionSelection.args = {
+    answerAmount: 3,
+    createDate: "2023-02-04T13:34:48.551Z",
     title: "You strive for victory",
 };
